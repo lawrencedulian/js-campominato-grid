cdb09211daf6d5885303052c117c123e9096ee49
lawrencedulian <lawrenceedulian@gmail.com>
2022-10-18 15:33:23 +0200

    completed script.js and html

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-
+// Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 
 const gridElement = document.getElementById("grid");
-// 1. Generare numeri da 1 a 100 e stamparli all'interno della cella.
-for (let i = 1; i <= 100; i++ ) {
-    console.log(i);
-    const cellElement = `<div class="square"> ${i} </div>`
-    gridElement.innerHTML += cellElement; 
+document.getElementById("play-btn").addEventListener("click", startGame);
+gridElement.innerHTML = "";
+
+
+
+function startGame () {
+for (let i = 1; i <= 100; i++) {
+    const newSquare = createSquare (i);
+    newSquare.addEventListener("click", onClickColor);
+    gridElement.append(newSquare);
+}
+gridElement.classList.remove("d-none");
 }
 
+/**
+ * Description: Funzione che crea un elemento html che rappresenta un quadrato della griglia
+ * @param {number} innerNumber - numero da mostrare nel quadrato
+ * @returns {object} Elemento Html del quadrato
+ */
+function createSquare(innerNumber) {
+    const square = document.createElement("div");
+    square.innerHTML = `<span>${innerNumber}</span>`
+    square.classList.add("square");
+    return square;
+}
 
-// 2. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-const squareElement = document.getElementsByClassName ("square");
-
-squareElement.addEventListener ("click", function (
-    
-))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Description: Funzione che gestisce il click sullo square
+ */
+function onClickColor() {
+    console.log(parseInt(this.textContent));
+    this.classList.toggle("bg-blue");
+}
